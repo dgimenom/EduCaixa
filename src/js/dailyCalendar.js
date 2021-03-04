5f3f5fa3b4df15d1746faacb2c95d7635c39533d
dgimenom <david.gimeno.mane@everis.com>
2021-03-04 09:23:21 +0100

    chore: adapt data structure + popup fixes

--- src/js/dailyCalendar.js
+++ src/js/dailyCalendar.js
@@ -11,23 +11,17 @@ const data = [
           {
             startTime: "09:30",
             endTime: "11:30",
-            offset: "2.78rem",
-            range: "11.11rem",
             remaining: "11 places disponibles",
           },
           {
             startTime: "12:30",
             endTime: "14:30",
-            offset: "19.4rem",
-            range: "11.11rem",
             remaining: "22 places disponibles",
           },
           {
             empty: true,
             startTime: "16:00",
             endTime: "18:00",
-            offset: "38.8rem",
-            range: "11.11rem",
             remaining: "33 places disponibles",
           },
         ],
@@ -41,8 +35,6 @@ const data = [
           {
             startTime: "09:00",
             endTime: "18:00",
-            offset: "0rem",
-            range: "11.11rem",
             remaining: "11 places disponibles",
           },
         ],
@@ -53,38 +45,92 @@ const data = [
     date: "04/25/2015",
     activities: [
       {
-        id: "1",
-        title: "Comunica´t. Taller d´expressió corporal",
-        maximum: "Máx 230 alumnes",
-        price: "35$ per grup",
+        id: "2",
+        title: "Comunica´t. Taller d´expressió corporal 2",
+        maximum: "Máx 30 alumnes",
+        price: "25$ per grup",
         events: [
           {
-            startTime: "09:30",
-            endTime: "11:30",
-            offset: "2.78rem",
-            range: "11.11rem",
-            remaining: "11 places disponibles",
-          },
-          {
-            startTime: "12:30",
-            endTime: "14:30",
-            offset: "19.4rem",
-            range: "11.11rem",
-            remaining: "22 places disponibles",
-          },
-          {
-            empty: true,
-            startTime: "16:00",
+            startTime: "09:00",
             endTime: "18:00",
-            offset: "38.8rem",
-            range: "11.11rem",
-            remaining: "33 places disponibles",
+            remaining: "11 places disponibles",
           },
         ],
       },
     ],
   },
 ];
+
+// const data2 = 
+// [
+//   {​
+//     date: "03/03/2021",
+//     week: true,
+//     center: "CaixaForum Barcelona",
+//     groups: [
+//       {
+//         ​
+//         id: 1,
+//         name: "Bachillerato A",
+//       }​,
+//       {
+//         ​
+//         id: 2,
+//         name: "Bachillerato B",
+//       }​
+//     ],
+//     activities: [
+//       {      ​
+//         fblcActivityId: "11111",
+//         eventId: "11111",
+//         title: "Comunica´t. Taller d´expressió corporal",
+//         imageUrl: "http://www.google.es",
+//         activityPlanId: "1111",
+//         friendlyUrl: "http://www.google.es",
+//         product: {
+//           ​
+//           centerName: "",
+//           date: "",
+//           capacity: "Máx 230 alumnes",
+//           price: "35$ per grup",
+//           locationId: "",
+//         }​,
+//         matched: true,
+//         promotions: [
+//           {​
+//             id: 1,
+//             name: "Taller Comunicate + Visita guiada 50% descuento",
+//             status: "ACTIVE",
+//             type: "PROMOTION",
+//             startDate: "yyyy-MM-ddThh:mm:ss",
+//             endDate: "yyyy-MM-ddThh:mm:ss",
+//           }, 
+//         ​],
+//         sessions: [
+//           {
+//             startDatetime: "yyyy-MM-dd'T'hh:mm:ss",
+//             endDatetime: "yyyy-MM-dd'T'hh:mm:ss",
+//             availability: 11,
+//             maxCapacity: 40,
+//           }​,
+//           { ​
+//             startDatetime: "yyyy-MM-dd'T'hh:mm:ss",
+//             endDatetime: "yyyy-MM-dd'T'hh:mm:ss",
+//             availability: 11,
+//             maxCapacity: 40,
+//           }​,
+//           { ​
+//             startDatetime: "yyyy-MM-dd'T'hh:mm:ss",
+//             endDatetime: "yyyy-MM-dd'T'hh:mm:ss",
+//             availability: 11,
+//             maxCapacity: 40,
+//           }​,
+//         ],
+//       },​
+//     ],
+//   },
+// ];
+
 const templateConst = `...`;
 const CALENDAR_HEIGHT = 49.375;
 const TIME_SLOTS = 9;
@@ -126,8 +172,15 @@ function toggleSessions(id) {
 
 function updateData() {
   const randomDay = Math.floor(Math.random() * data.length);
-  console.log('RANDOM DAY: ' + data[randomDay].date);
   renderElements(data[randomDay]);
 }
 
 renderElements(data[0]);
+
+$(".calendar__modal-link").on("click", function () {
+  $(this).toggleClass("open");
+});
+
+$(".calendar__modal-close").on("click", function () {
+  $(".calendar__modal-link").removeClass("open");
+});
